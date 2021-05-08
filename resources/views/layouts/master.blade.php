acb208ea354c67b1f0533112682a42fb960b1494
farendi <farendigiotivano@unesa.ac.id>
2021-05-08 20:26:28 +0700

    fix login dan dashboard

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -119,9 +119,9 @@
   <!-- /.content-wrapper -->
   <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.0.5
+      <b>-</b>
     </div>
-    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong> All rights
+    <strong>Copyright &copy; {{date('Y')}} <a href="#">Email</a>.</strong> All rights
     reserved.
   </footer>
 
